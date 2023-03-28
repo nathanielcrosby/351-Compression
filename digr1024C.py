2526df8137a997804ced189782c833ae33726fa7
dndang23 <dndang23@amherst.edu>
2023-03-28 08:39:17 -0400

    I.A.P.I.M.C.Y!

---
+++ digr1024C.py
@@ -0,0 +1,52 @@
+import pickle
+import sys
+
+def encode(code, data):
+    value = ""
+    i = 0
+    while i < len(data):
+        if i == (len(data) - 1):
+            value += code[data[i]]
+            i += 1
+        elif code.get(data[i:i+2]) is not None:
+            value += code[data[i:i+2]]
+            i += 2
+        elif code.get(data[i]) is not None:
+            value += code[data[i]]
+            i += 1
+        else:
+            i += 1
+
+    return value
+
+def str_to_list(value):
+    vals = []
+    for i in range(0, len(value), 10):
+        if (i < (len(value) - 9)):
+            vals.append(int(value[i:i+10], 2))
+        else:
+            vals.append(int(value[i:], 2) * (2 ** (10 - (len(value) % 10))))
+
+    return vals
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        print("Incorrect number of arguments. Input: python digr1024C.py filename")
+
+    else:
+        filename = sys.argv[1]
+        try:
+            my_file = open(filename, 'r', encoding="utf8")
+            data = my_file.read()
+        except FileNotFoundError:
+            print("No file found with name: ", filename)
+            print("Ensure correct arguments. Input: python digr1024C.py filename")
+
+        with open('digram_code1024.pkl', 'rb') as f:
+            code = pickle.load(f)
+
+        value = encode(code, data)
+        #vals = str_to_list(value)
+
+        with open(filename+'.digr1024', 'w') as f:
+            f.write(value)
